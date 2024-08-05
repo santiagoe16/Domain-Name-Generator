938e0a441604039e524b2c9bd321a9a017667495
santiagoe16 <santiagoespinal333@gmail.com>
2024-08-05 00:26:13 +0000

    Domain name generator finish

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+let domainNames = [];
+
+// Recorre cada pronombre
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      let domain = pronoun[i] + adj[a] + noun[n];
+      domainNames.push(domain + ".com");
+    }
+  }
+}
+
+// Imprime los nombres de dominio generados
+domainNames.forEach(domain => {
+  console.log(domain);
+});
