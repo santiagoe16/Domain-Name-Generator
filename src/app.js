65acfdabeb49fbb1bd3b6676856671ff5ff27634
santiagoe16 <santiagoespinal333@gmail.com>
2024-08-08 02:03:11 +0000

    refactored

--- src/app.js
+++ src/app.js
@@ -9,19 +9,10 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-let domainNames = [];
-
-// Recorre cada pronombre
 for (let i = 0; i < pronoun.length; i++) {
   for (let a = 0; a < adj.length; a++) {
     for (let n = 0; n < noun.length; n++) {
-      let domain = pronoun[i] + adj[a] + noun[n];
-      domainNames.push(domain + ".com");
+      console.log(pronoun[i] + adj[a] + noun[n] + ".com");
     }
   }
 }
-
-// Imprime los nombres de dominio generados
-domainNames.forEach(domain => {
-  console.log(domain);
-});
